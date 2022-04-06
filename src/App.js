5d9f1ef73054d87248ae6e5dac0107046ffdd08f
davidfdzmorilla <davidfdzmorilla@gmail.com>
2022-04-06 17:10:14 +0200

    restructuring App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Epic from './components/Epic';
+import Epic from './pages/Epic';
 import Header from './components/Header';
-import MarsRoverPhotos from './components/MarsRoverPhotos';
-import PictureOfTheDay from './components/PictureOfTheDay';
+import MarsRoverPhotos from './pages/MarsRoverPhotos';
+import PictureOfTheDay from './pages/PictureOfTheDay';
 import ErrorBoundary from './utils/ErrorBoundary';
 
 function App() {
